2fe80152963fd552a6b48fc4f9b41c87ed849fe3
Deepika Ankareddi <ankareddydeepika@gmail.com>
2020-05-02 18:42:37 -0400

    test

--- ReadMe.md
+++ ReadMe.md
@@ -1,23 +1,23 @@
-null
+Deepika Ankareddi
+![profilepic](https://avatars1.githubusercontent.com/u/61395542?v=4)
 
-  # djdjk
+  # 
   ## Description 
- 	 dhihdl
+ 	 
   ## Table of Contents 
  	
   * TOC
    {:toc}
   ## Installations 
- 	 dkkd
+ 	 
   ## Usage 
- 	 akdgk
+ 	 
   ## License
- 	 afdgk
+ 	 
   ## Contributing 
- 	 dsgdkchd
+ 	 
   ## Tests 
- 	 acjdsgck
+ 	 
   
   
-![profilepic](https://avatars1.githubusercontent.com/u/61395542?v=4)
-Deepika Ankareddi
+null
--- utils/api.js
+++ utils/api.js
@@ -8,16 +8,14 @@ const api = {
 
    const queryUrl = `https://api.github.com/users/${data.name}`;
    axios.get(queryUrl).then(function(response){
-    
+    const result = markdown.generateMarkdown(data)
+     writeFile.writeToFile(result);
     const emailId = response.data.email;
     const profileimg = response.data.avatar_url;
     const profilename = response.data.name;
     writeFile.writeToFile(emailId);
     writeFile.writeToFile(`![profilepic](${profileimg})`);
     writeFile.writeToFile(profilename);
-    const result = markdown.generateMarkdown(data)
-     writeFile.writeToFile(result);
-
 });
 
   }
